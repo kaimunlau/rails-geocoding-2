f81641e2df1528f0cdee511179a9fe8889526dde
david lau <david.kaimun.lau@gmail.com>
2025-03-05 09:31:48 +0100

    feat: add geocoding to flats

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_05_075544) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_05_081931) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,5 +19,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_05_075544) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 end
