c4574f1a8f23afc638c55620fd1d61fa90f8b91f
david lau <david.kaimun.lau@gmail.com>
2025-03-05 10:35:17 +0100

    feat: add popup to markers

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -7,7 +7,11 @@ class FlatsController < ApplicationController
     @markers = @flats.geocoded.map do |flat|
       {
         lat: flat.latitude,
-        lng: flat.longitude
+        lng: flat.longitude,
+        info_window_html: render_to_string(
+          partial: "info_window",
+          locals: { flat: flat }
+        )
       }
     end
   end
